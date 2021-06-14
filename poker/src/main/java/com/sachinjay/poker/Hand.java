aa6074ddfd4b61976abc820e591e2ed9e9dc986f
Sachin Devasahayam <sjd232@cornell.edu>
2021-06-14 18:39:13 -0400

    Started Hand class

--- poker/src/main/java/com/sachinjay/poker/Hand.java
+++ poker/src/main/java/com/sachinjay/poker/Hand.java
@@ -2,5 +2,15 @@ package com.sachinjay.poker;
 
 public class Hand
 {
+	/**
+	 * Array of cards that makes up the hand
+	 * 
+	 * INVARIANT: hand.length == 5
+	 */
+	private Card[] hand;
 
+	public void Hand(Card[] hand)
+	{
+		this.hand = hand;
+	}
 }
