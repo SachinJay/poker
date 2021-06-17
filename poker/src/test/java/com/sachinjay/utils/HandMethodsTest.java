12735c22b7c3650a887d43dd104b051323f9ddec
Sachin Devasahayam <sjd232@cornell.edu>
2021-06-17 18:21:35 -0400

    Init'ed test file to test hand type classification

---
+++ poker/src/test/java/com/sachinjay/utils/HandMethodsTest.java
@@ -0,0 +1,16 @@
+package com.sachinjay.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HandMethodsTest
+{
+
+	@Test
+	public void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
